67cce5df473e55edeaa3ca54948ac81fe68f5e3f
Irina Gudova <melioraa@gmail.com>
2015-03-25 00:51:20 +0300

    pdf

---
+++ sertificator/sertificator/CreatePDF.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Mime;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+
+namespace sertificator
+{
+    class CreatePDF
+    {
+        public static void Create()
+        {
+            //Console.WriteLine("Введите путь, по которому будет размещен сгенерированный PDF-файл:");
+            //string filePath = Console.ReadLine();
+            string filePath = @"c://users/lenovo/documents";
+            string imagePath = @"c://users/lenovo/documents";
+            //size of document        
+            using (FileStream fs = new FileStream(filePath+@"/rkkррррррррmd.pdf", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                Rectangle pagesize = new Rectangle(PageSize.A5.Rotate()); //size of document
+                Document doc = new Document(pagesize, 0f, 0f, 0f, 0f);
+                PdfWriter writer = PdfWriter.GetInstance(doc, fs);
+                doc.Open();
+                //doc.Add(new Paragraph("JPG"));
+                Image jpg = Image.GetInstance(imagePath+@"/image.jpg");
+                jpg.ScaleToFit(610f, 420f);
+                //jpg.Alignment = Element.ALIGN_CENTER;
+                doc.Add(jpg);
+                doc.Add(new Paragraph("Привет!"));
+                doc.Close();
+            }
+
+        }
+    }
+}
--- sertificator/sertificator/Program.cs
+++ sertificator/sertificator/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace sertificator
+﻿using System;
+namespace sertificator
 {
     class Program
     {
         static void Main(string[] args)
         {
+            CreatePDF.Create();
+           Console.WriteLine();
         }
     }
 }
